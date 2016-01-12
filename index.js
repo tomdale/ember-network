81ada2885fdf648b85435a33549ec80d39e2b569
Tom Dale <tom@tomdale.net>
2016-01-12 18:51:00 -0500

    Make dynamic dependencies more future-proof
    This removes the reliance on the `reexports` path which is going away.

--- index.js
+++ index.js
@@ -8,25 +8,52 @@ var stew = require('broccoli-stew');
 var rename = stew.rename;
 var find = stew.find;
 var Template = require('broccoli-templater');
-var mergeTrees = require('broccoli-merge-trees');
-var Funnel = require('broccoli-funnel');
+var funnel = require('broccoli-funnel');
 
 module.exports = {
   name: 'ember-network',
 
-  treeForAddon: function(tree) {
-    var addonTree = this._super.treeForAddon.call(this, tree);
-    var fetchTree;
+  included: function(app) {
+    app.import('vendor/fetch.js');
+  },
 
+  treeForVendor: function() {
     if (isFastBoot()) {
-      fetchTree = treeForNodeFetch();
+      return treeForNodeFetch();
     } else {
-      fetchTree = treeForBrowserFetch();
+      return treeForBrowserFetch();
     }
-
-    return mergeTrees([ addonTree, fetchTree ]);
   }
 };
+
+function isFastBoot() {
+  return process.env.EMBER_CLI_FASTBOOT === 'true';
+}
+
+function treeForNodeFetch() {
+  return normalizeFileName(funnel(path.join(__dirname, './assets'), {
+    files: ['fastboot-fetch.js'],
+  }));
+}
+
+function treeForBrowserFetch() {
+  var fetchPath = require.resolve('whatwg-fetch');
+  var expandedFetchPath = expand(fetchPath);
+
+  var fetch = normalizeFileName(find(expandedFetchPath));
+
+  return new Template(fetch, templatePath, function(content) {
+    return {
+      moduleBody: content
+    };
+  });
+}
+
+function normalizeFileName(tree) {
+  return rename(tree, function() {
+    return 'fetch.js';
+  });
+}
 
 function expand(input) {
   var dirname = path.dirname(input);
@@ -35,28 +62,3 @@ function expand(input) {
   return dirname + '/{' + file + '}';
 }
 
-function isFastBoot() {
-  return process.env.EMBER_CLI_FASTBOOT === 'true';
-}
-
-function treeForNodeFetch() {
-  return new Funnel(path.join(__dirname, './assets'), {
-    files: ['fastboot-fetch.js'],
-    destDir: 'reexports'
-  });
-}
-
-function treeForBrowserFetch() {
-  var fetchPath = require.resolve('whatwg-fetch');
-  var expandedFetchPath = expand(fetchPath);
-
-  var fetch = rename(find(expandedFetchPath), function() {
-    return 'reexports/fetch.js';
-  });
-
-  return new Template(fetch, templatePath, function(content) {
-    return {
-      moduleBody: content
-    };
-  });
-}
